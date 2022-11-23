40fbae87d4f3e6980fd7e92c8dc278bda3456c52
Azharul Islam <lonewolfazharul@gmail.com>
2022-11-24 03:21:40 +0600

    simple banner navbar & footer added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    // Banner
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <img className='w-6/12' src='https://i.ibb.co/h1QJgqC/a7ab72b9e682ccda79bea599ffd8d3c9.gif' alt='I-phone animating' />
+                <div>
+                    <h1 className="text-5xl font-bold">Resale Treasury</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
