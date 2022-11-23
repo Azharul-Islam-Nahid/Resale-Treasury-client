4895c9d4acd73591ee09927de0d624f0888277da
Azharul Islam <lonewolfazharul@gmail.com>
2022-11-24 04:45:20 +0600

    login & signup page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+
+
+    const handleLogin = data => {
+        console.log("🚀 ~ file: Login.js ~ line 12 ~ handleLogin ~ data", data)
+
+    }
+
     return (
-        <div>
-            login
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='font-extrabold text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="text"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        <label className="label"> <span className="label-text"></span></label>
+                    </div>
+                    <input className='btn btn-info w-full' value="Login" type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
+                </form>
+                <p>New to Resale Treasury? <Link className='font-bold text-info' to="/signup">Create New Account</Link></p>
+                <div className="divider"> </div>
+                <button className='btn btn-info btn-outline w-full'>Google Log In</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [signupError, setSignupError] = useState('');
+    const [createUserEmail, setCreateUserEmail] = useState('');
+
+
+    const handleSignup = data => {
+        console.log("🚀 ~ file: Signup.js ~ line 12 ~ handleSignup ~ data", data)
+
+    }
+
     return (
-        <div>
-            signup
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='font-extrabold text-xl text-center'>Signup</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="text"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <label className="label"> <span className="label-text">Account Type</span></label>
+                    <select name="select" className="select select-bordered w-full max-w-xs"
+                        {...register("role", {
+                        })}>
+                        <option defaultValue>Buyer</option>
+                        <option>Seller</option>
+                    </select>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        <label className="label"> <span className="label-text"></span></label>
+                    </div>
+                    <input className='btn btn-info w-full' value="signup" type="submit" />
+                    <div>
+                        {signupError && <p className='text-red-600'>{signupError}</p>}
+                    </div>
+                </form>
+                <p>Already have an account? <Link className='font-bold text-info' to="/signup">signup</Link></p>
+                <div className="divider"> </div>
+                <button className='btn btn-info btn-outline w-full'>Google Log In</button>
+            </div>
         </div>
     );
 };
