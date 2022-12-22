6be1dcead0bdafa49d26d958034ebd5caa6f2632
Azharul Islam <lonewolfazharul@gmail.com>
2022-12-22 15:02:50 +0600

    login redirect bug fixed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider } from 'firebase/auth';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -24,9 +24,15 @@ const Login = () => {
     const googleProvider = new GoogleAuthProvider();
     const from = location.state?.from?.pathname || '/';
 
-    if (token) {
-        navigate(from, { replace: true });
-    }
+
+    useEffect(() => {
+        if (token) {
+            navigate(from, { replace: true });
+        }
+    }, [from, token, navigate])
+
+
+
 
 
 
